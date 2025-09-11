adb9d2a6227a955461345c5226dc6d575b23acc1
Ashlyn Joshy <ashlyntherese@gmail.com>
2025-09-11 08:09:38 +0530

    connection to the mongodb

--- readingroom-be/server.js
+++ readingroom-be/server.js
@@ -1,16 +1,26 @@
-const express = require('express');
-require('dotenv').config();
-
+const express = require("express");
+require("dotenv").config();
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.get ('/', (req, res) => {
-    res.send('Hello World! This is a backend server for Reading Room application.');
-    next();
+app.get("/", (req, res) => {
+  res.send(
+    "Hello World! This is a backend server for Reading Room application."
+  );
+  next();
 });
 
 const port = process.env.PORT || 8000;
 
-app.listen(port, () =>{
-    console.log(`Server is running on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+  mongoose.connect(process.env.MONGODB_URI).then(
+    () => {
+      console.log("Connected to MongoDB");
+    },
+    (err) => {
+      console.log("Error connecting to MongoDB:", err);
+    }
+  );
+});
