817a44186da67c1666f0b956108f73abb9e87cc8
Ashlyn Joshy <ashlyntherese@gmail.com>
2025-09-17 14:19:10 +0530

    remove the sub category from the main category

--- readingroom-be/controllers/mainCategory.js
+++ readingroom-be/controllers/mainCategory.js
@@ -52,3 +52,26 @@ module.exports.addSubCategoryToMainCategory = async (req, res) => {
         res.status(500).json({ error: 'Error adding sub-category to main-category', details: error.message });
     }
 }
+
+//remove a sub-category from a main-category
+module.exports.removeSubCategoryFromMainCategory = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { subCategoryId } = req.body;
+
+        //validate ids
+        if (!mongoose.Types.ObjectId.isValid(id) || !mongoose.Types.ObjectId.isValid(subCategoryId)) {
+            return res.status(400).json({ error: 'Invalid ID format' });
+        }
+
+        const mainCategory = await MainCategory.findById(id);
+        if (!mainCategory) {
+            return res.status(404).json({ error: 'Main-category not found' });
+        }
+        mainCategory.SubCategory = mainCategory.SubCategory.filter(subCatId => subCatId.toString() !== subCategoryId);
+        await mainCategory.save();
+        res.status(200).json(mainCategory);
+    } catch (error) {
+        res.status(500).json({ error: 'Error removing sub-category from main-category', details: error.message });
+    }
+}
--- readingroom-be/routers/MainCategory.js
+++ readingroom-be/routers/MainCategory.js
@@ -7,5 +7,6 @@ const MainCategory = require("../controllers/mainCategory");
 router.post('/', MainCategory.createMainCategory);
 router.get('/all', MainCategory.getAllMainCategories);
 router.put('/:id/addSubCategory', MainCategory.addSubCategoryToMainCategory);
+router.put('/:id/removeSubCategory', MainCategory.removeSubCategoryFromMainCategory);
 
 module.exports = router;
