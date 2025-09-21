bb06c1d773f717e5078ffb9e09f0cd55a0eab028
Ashlyn Joshy <ashlyntherese@gmail.com>
2025-09-22 01:10:27 +0530

    update user details

--- readingroom-be/controllers/user.js
+++ readingroom-be/controllers/user.js
@@ -53,3 +53,26 @@ module.exports.getAllAdmins = async (req, res) => {
         res.status(500).json({ message: 'Error in fetching admins', error: error.message });
     }
 }
+
+//update user details
+module.exports.updateUser = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const { username, email } = req.body;
+        const checkExistingUsername = await User.findOne({ username, _id: { $ne: id } });
+        if(checkExistingUsername) {
+            return res.status(400).json({ message: 'Username already exists' });
+        }
+        const checkExistingEmail = await User.findOne({ email, _id: { $ne: id } });
+        if(checkExistingEmail) {
+            return res.status(400).json({ message: 'Email already exists' });
+        }
+        const user = await User.findByIdAndUpdate(id, { username, email }, { new: true }).select('-password');
+        if(!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        res.status(200).json({ message: 'User details updated successfully', user });
+    } catch (error) {
+        res.status(500).json({ message: 'Error in updating user details', error: error.message });
+    }
+}
--- readingroom-be/routers/user.js
+++ readingroom-be/routers/user.js
@@ -8,5 +8,6 @@ router.post('/createuser', Users.createUser);
 router.get('/allusers', Users.getAllUsers);
 router.get('/alladmins', Users.getAllAdmins);
 router.get('/:id', Users.getUserDetails);
+router.put('/:id/update', Users.updateUser);
 
 module.exports = router;
